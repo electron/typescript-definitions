6daebafec51193f6ffee5e7362e833553a8cb841
Samuel Attard <samuel.r.attard@gmail.com>
2016-11-03 14:11:18 +1100

    Node v4 compatible

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 require('colors')
-const { extendArray } = require('./utils')
-const { flushParamInterfaces } = require('./lib/dynamic-param-interfaces')
+const utils = require('./utils')
+const paramInterfaces = require('./lib/dynamic-param-interfaces')
 const generateMasterInterfaces = require('./lib/master-interfaces')
-const { generateModuleDeclaration, getModuleDeclarations } = require('./lib/module-declaration')
+const moduleDeclaration = require('./lib/module-declaration')
 const remapOptionals = require('./lib/remap-optionals')
 
 module.exports = (API) => {
   const outputLines = []
 
-  const addThing = (lines, sep = '') => extendArray(outputLines, lines.map((l, i) => (i === 0 || i >= lines.length - 2) ? l : `${l}${sep}`).concat(['\n']))
+  const addThing = (lines, sep = '') => utils.extendArray(outputLines, lines.map((l, i) => (i === 0 || i >= lines.length - 2) ? l : `${l}${sep}`).concat(['\n']))
 
   remapOptionals(API)
   generateMasterInterfaces(API, addThing)
 
   API.sort((m1, m2) => m1.name.localeCompare(m2.name)).forEach((module, index) => {
-    generateModuleDeclaration(module, index, API)
+    moduleDeclaration.generateModuleDeclaration(module, index, API)
   })
 
-  Object.keys(getModuleDeclarations()).forEach((moduleKey) => {
-    const moduleAPI = getModuleDeclarations()[moduleKey]
+  Object.keys(moduleDeclaration.getModuleDeclarations()).forEach((moduleKey) => {
+    const moduleAPI = moduleDeclaration.getModuleDeclarations()[moduleKey]
     moduleAPI.push('}')
     addThing(moduleAPI.map((l, index) => (index === 0 || index === moduleAPI.length - 1) ? l : `  ${l}`))
   })
 
-  flushParamInterfaces(API, addThing)
+  paramInterfaces.flushParamInterfaces(API, addThing)
 
   return outputLines
 }
