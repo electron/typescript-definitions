5db61295f9fc98ee221035bc16f45adbe742cc1d
Felix Rieseberg <felix@felixrieseberg.com>
2017-03-24 10:26:10 -0700

    :wrench: Mark EventEmitter’s methods as public

--- base/base_inner.ts
+++ base/base_inner.ts
@@ -1,17 +1,17 @@
   class EventEmitter {
-    addListener(event: string, listener: Function): this;
-    on(event: string, listener: Function): this;
-    once(event: string, listener: Function): this;
-    removeListener(event: string, listener: Function): this;
-    removeAllListeners(event?: string): this;
-    setMaxListeners(n: number): this;
-    getMaxListeners(): number;
-    listeners(event: string): Function[];
-    emit(event: string, ...args: any[]): boolean;
-    listenerCount(type: string): number;
-    prependListener(event: string, listener: Function): this;
-    prependOnceListener(event: string, listener: Function): this;
-    eventNames(): string[];
+    public addListener(event: string, listener: Function): this;
+    public on(event: string, listener: Function): this;
+    public once(event: string, listener: Function): this;
+    public removeListener(event: string, listener: Function): this;
+    public removeAllListeners(event?: string): this;
+    public setMaxListeners(n: number): this;
+    public getMaxListeners(): number;
+    public listeners(event: string): Function[];
+    public emit(event: string, ...args: any[]): boolean;
+    public listenerCount(type: string): number;
+    public prependListener(event: string, listener: Function): this;
+    public prependOnceListener(event: string, listener: Function): this;
+    public eventNames(): string[];
   }
 
   class Accelerator extends String {
