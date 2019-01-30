316eb8f484914282da5e0d33b40bee2a7eb5d979
Nitish Sakhawalkar <nitsakh@icloud.com>
2019-01-30 09:46:50 -0800

    fix: Update tests for new schemes api (#125)
    BREAKING CHANGE: Update tests for new schemes api
    
    * update electron commit

--- vendor/fetch-docs.js
+++ vendor/fetch-docs.js
@@ -7,7 +7,7 @@ const mkdirp = require('mkdirp').sync
 const os = require('os')
 
 const downloadPath = path.join(os.tmpdir(), 'electron-api-tmp')
-const ELECTRON_COMMIT = '0c96f14a5b702036559a8012ef0dd9649221d5d9'
+const ELECTRON_COMMIT = '1a93b81a10fa63fce2e404ae71c0289d373e08f4'
 
 rm(downloadPath)
 
