6d3db61fff510797525f035c86c34c4a18f68581
Linda_pp <rhysd@users.noreply.github.com>
2023-02-02 18:19:02 +0900

    fix: make Accelerator alias of builtin string type (#179)
    Co-authored-by: Samuel Attard <sam@electronjs.org>

--- base/base_inner.ts
+++ base/base_inner.ts
@@ -1,10 +1,9 @@
   const NodeEventEmitter: typeof import('events').EventEmitter;
 
+  type Accelerator = string;
   type EmptyParams = {};
   type Event<Params extends object, Sender extends NodeJS.EventEmitter> = {
     preventDefault: () => void;
     readonly defaultPrevented: boolean;
     sender: Sender;
   } & Params;
-
-  class Accelerator extends String {}
