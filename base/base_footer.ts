b82285945836cca988d38fefc679748b890cd0ab
dlguswo333 <dlguswo002@gmail.com>
2024-11-09 05:21:00 +0900

    fix: remove File.path (#281)

--- base/base_footer.ts
+++ base/base_footer.ts
@@ -34,13 +34,6 @@ interface NodeRequire {
   (moduleName: 'electron/utility'): typeof Electron.Utility;
 }
 
-interface File {
-  /**
-   * The real path to the file on the users filesystem
-   */
-  path: string;
-}
-
 declare module 'original-fs' {
   import * as fs from 'fs';
   export = fs;
